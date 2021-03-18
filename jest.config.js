739bad4534b5dcf28759dd43a785e7a4891700c4
bielziiN <gabrielcerqueira96@hotmail.com>
2021-03-17 21:12:49 -0300

    chore: any

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,31 @@
 module.exports = {
-	verbose: true,
-	modulePathIgnorePatterns: ['dist']
+  verbose: true,
+  modulePathIgnorePatterns: ['dist'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  coverageReporters:[
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
